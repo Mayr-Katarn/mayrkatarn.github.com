e84c93ee568c77c59011bfa1cbf5b0592e8f0c66
Mayr-Katarn <redtein77@live.com>
2020-10-19 18:19:42 +0700

    new

--- simon/deploy.sh
+++ simon/deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Mayr-Katarn/mayrkatarn.github.com.git master:gh-pages
+git push -f git@github.com:Mayr-Katarn/mayrkatarn.pages.git master:gh-pages
 
 cd -
